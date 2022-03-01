bea72bec07145862f5b1218aa6f1db4dc6ebdd80
tejo <m.parmi@gmail.com>
2022-03-01 15:40:40 +0100

    Add flatten challenge.

--- lib/flatten.rb
+++ lib/flatten.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Flatten
+  @return_array = []
+  def self.to_flat(array)
+    array.each do |element|
+      if element.is_a? Array
+        to_flat(element)
+      else
+        @return_array << element
+      end
+    end
+    @return_array
+  end
 end
--- spec/flatten_spec.rb
+++ spec/flatten_spec.rb
@@ -5,5 +5,6 @@ require 'flatten'
 RSpec.describe Flatten do
   context '.to_flat'
   it 'flatten an array' do
+    expect(Flatten.to_flat([1, [2, [3]], 4])).to eq([1, 2, 3, 4])
   end
 end
